2db6c59aa59e76899cc7026958107591fbb663d6
Scott Brookes <sbrookes@draper.com>
2019-06-10 21:42:33 -0400

    example of how to build the ISP backend

--- llvm/lib/Target/RISCV/RISCVAsmPrinter.cpp
+++ llvm/lib/Target/RISCV/RISCVAsmPrinter.cpp
@@ -14,13 +14,11 @@
 #include "RISCV.h"
 #include "InstPrinter/RISCVInstPrinter.h"
 #include "MCTargetDesc/RISCVMCExpr.h"
-#include "MCTargetDesc/RISCVELFStreamer.h"
 #include "RISCVTargetMachine.h"
 #include "llvm/CodeGen/AsmPrinter.h"
 #include "llvm/CodeGen/MachineConstantPool.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineInstr.h"
-#include "llvm/CodeGen/MachineInstrBuilder.h"
 #include "llvm/CodeGen/MachineModuleInfo.h"
 #include "llvm/MC/MCAsmInfo.h"
 #include "llvm/MC/MCInst.h"
@@ -28,51 +26,7 @@
 #include "llvm/MC/MCSymbol.h"
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/raw_ostream.h"
-
-//SSITH Extra includes
-#include "MCTargetDesc/RISCVFixupKinds.h"
-#include "llvm/BinaryFormat/ELF.h"
-#include "llvm/MC/MCAsmBackend.h"
-#include "llvm/MC/MCFixupKindInfo.h"
-#include "llvm/MC/MCFixup.h"
-#include "llvm/MC/MCSectionELF.h"
-#include "llvm/MC/MCContext.h"
-#include "llvm/MC/SSITHMetadata.h"
-#include "llvm/Target/TargetLoweringObjectFile.h"
-
-using namespace llvm;
-
-#define DEBUG_TYPE "asm-printer"
-
-namespace {
-class RISCVAsmPrinter : public AsmPrinter {
-public:
-  explicit RISCVAsmPrinter(TargetMachine &TM,
-                           std::unique_ptr<MCStreamer> Streamer)
-      : AsmPrinter(TM, std::move(Streamer)) {}
-
-  StringRef getPassName() const override { return "RISCV Assembly Printer"; }
-
-  void EmitInstruction(const MachineInstr *MI) override;
-
-  bool PrintAsmOperand(const MachineInstr *MI, unsigned OpNo,
-                       const char *ExtraCode, raw_ostream &OS) override;
-  bool PrintAsmMemoryOperand(const MachineInstr *MI, unsigned OpNo,
-                             const char *ExtraCode, raw_ostream &OS) override;
-
-  bool EmitToStreamer(MCStreamer &S, const MCInst &Inst);
-  bool emitPseudoExpansionLowering(MCStreamer &OutStreamer,
-                                   const MachineInstr *MI);
-
-  //SSITH Addition
-  void EmitSSITHMetadataFnRange(MCSymbol *begin, MCSymbol *end) override;
-  
-  // Wrapper needed for tblgenned pseudo lowering.
-  bool lowerOperand(const MachineOperand &MO, MCOperand &MCOp) const {
-    return LowerRISCVMachineOperandToMCOperand(MO, MCOp, *this);
-  }
-};
-}
+#include "RISCVAsmPrinter.h"
 
 #define GEN_COMPRESS_INSTR
 #include "RISCVGenCompressInstEmitter.inc"
@@ -89,54 +43,14 @@ bool RISCVAsmPrinter::EmitToStreamer(MCStreamer &S, const MCInst &Inst) {
 // instructions) auto-generated.
 #include "RISCVGenMCPseudoLowering.inc"
 
-//SSITH
-void RISCVAsmPrinter::EmitSSITHMetadataFnRange(MCSymbol *begin, MCSymbol *end){
-  
-  SmallVector<MCFixup, 4> Fixups;
-
-  printf("Emitting SSITHMetadataFnRange\n");
-  
-  //Make MCExpr for the fixups -- Inspired by LowerSymbolOperand in RISCVMCInstLower.cpp
-  MCContext &Ctx = OutContext;
-  RISCVMCExpr::VariantKind Kind = RISCVMCExpr::VK_RISCV_None;
-  const MCExpr *MEbegin = MCSymbolRefExpr::create(begin, MCSymbolRefExpr::VK_None, Ctx);
-  const MCExpr *MEend = MCSymbolRefExpr::create(end, MCSymbolRefExpr::VK_None, Ctx);
-  MEbegin = RISCVMCExpr::create(MEbegin, Kind, Ctx);
-  MEend = RISCVMCExpr::create(MEend, Kind, Ctx);
-  
-  //Push fixup -- the linker will write the 4 byte address for us
-  Fixups.push_back(
-      MCFixup::create(0, MEbegin, MCFixupKind(FK_Data_4), SMLoc::getFromPointer(nullptr)));
-  Fixups.push_back(
-      MCFixup::create(0, MEend, MCFixupKind(FK_Data_4), SMLoc::getFromPointer(nullptr)));
-
-  OutStreamer->EmitLabel(end);
-  ((RISCVTargetELFStreamer*)(OutStreamer->getTargetStreamer()))->EmitSSITHMetadataEntry(Fixups, DMD_FUNCTION_RANGE, 0);
-}
-
 void RISCVAsmPrinter::EmitInstruction(const MachineInstr *MI) {
-
   // Do any auto-generated pseudo lowerings.
-  if (!emitPseudoExpansionLowering(*OutStreamer, MI)){
-    MCInst TmpInst;
-    LowerRISCVMachineInstrToMCInst(MI, TmpInst, *this);
-    EmitToStreamer(*OutStreamer, TmpInst);
-  }
+  if (emitPseudoExpansionLowering(*OutStreamer, MI))
+    return;
 
-  /* SSITH BEGIN */
-
-  // fn range avoids NoCFI on C code stuff
-  if(MI->isReturn()) 
-    EmitSSITHMetadataFnRange(CurrentFnSym, OutContext.createTempSymbol());
-
-  //SSITH - clean up in function epilog
-  if(MI->getFlag(MachineInstr::FnEpilog) && MI->getOpcode() == RISCV::LW){
-    //Emit our new store
-    MCInst SSITHStore;
-    LowerToSSITHEpilogStore(MI, SSITHStore, *this);
-    EmitToStreamer(*OutStreamer, SSITHStore);
-  }
-  
+  MCInst TmpInst;
+  LowerRISCVMachineInstrToMCInst(MI, TmpInst, *this);
+  EmitToStreamer(*OutStreamer, TmpInst);
 }
 
 bool RISCVAsmPrinter::PrintAsmOperand(const MachineInstr *MI, unsigned OpNo,
@@ -178,10 +92,4 @@ bool RISCVAsmPrinter::PrintAsmMemoryOperand(const MachineInstr *MI,
   }
 
   return AsmPrinter::PrintAsmMemoryOperand(MI, OpNo, ExtraCode, OS);
-}
-
-// Force static initialization.
-extern "C" void LLVMInitializeRISCVAsmPrinter() {
-  RegisterAsmPrinter<RISCVAsmPrinter> X(getTheRISCV32Target());
-  RegisterAsmPrinter<RISCVAsmPrinter> Y(getTheRISCV64Target());
 }
---
+++ llvm/lib/Target/RISCV/RISCVAsmPrinter.h
@@ -0,0 +1,50 @@
+//===-- RISCV.h - Top-level interface for RISCV -----------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the entry points for global functions defined in the LLVM
+// RISC-V back-end.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_RISCV_RISCVASMPRINTER_H
+#define LLVM_LIB_TARGET_RISCV_RISCVASMPRINTER_H
+
+#include "llvm/CodeGen/MachineInstr.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/CodeGen/AsmPrinter.h"
+
+#define DEBUG_TYPE "asm-printer"
+
+using namespace llvm;
+
+class RISCVAsmPrinter : public AsmPrinter {
+public:
+  explicit RISCVAsmPrinter(TargetMachine &TM,
+                           std::unique_ptr<MCStreamer> Streamer)
+      : AsmPrinter(TM, std::move(Streamer)) {}
+
+  StringRef getPassName() const override { return "RISCV Assembly Printer"; }
+
+  void EmitInstruction(const MachineInstr *MI) override;
+
+  bool PrintAsmOperand(const MachineInstr *MI, unsigned OpNo,
+                       const char *ExtraCode, raw_ostream &OS) override;
+  bool PrintAsmMemoryOperand(const MachineInstr *MI, unsigned OpNo,
+                             const char *ExtraCode, raw_ostream &OS) override;
+
+  bool EmitToStreamer(MCStreamer &S, const MCInst &Inst);
+  bool emitPseudoExpansionLowering(MCStreamer &OutStreamer,
+                                   const MachineInstr *MI);
+  
+  // Wrapper needed for tblgenned pseudo lowering.
+  bool lowerOperand(const MachineOperand &MO, MCOperand &MCOp) const {
+    return LowerRISCVMachineOperandToMCOperand(MO, MCOp, *this);
+  }
+};
+
+#endif
